802b3d7b33215d44159f4ad8e3c79a424cbf49e1
nooh saeed <noohsaeed240@gmail.com>
2025-06-30 13:33:18 +0300

    finsh dash board tablet layout

--- lib/widgets/dashboard_tablet_layout.dart
+++ lib/widgets/dashboard_tablet_layout.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess.dart';
+import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_cards_and_transction_history_section.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
@@ -9,14 +12,29 @@ class DashboardTabletLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Row(
       children: [
-        SizedBox(
-          width: 250,
-          child: CustomDrawer(),
+        Expanded(child: CustomDrawer()),
+            SizedBox(
+          width: 32,
         ),
         Expanded(
+          flex: 3,
           child: Padding(
-            padding: EdgeInsets.all(24.0),
-            child: AllExpenses(),
+            padding: EdgeInsets.only(top: 40),
+            child:  SingleChildScrollView(
+      child: Column(
+        children: [
+          AllExpensessAndQuickInvoiceSection(),
+          SizedBox(
+            height: 24,
+          ),
+          MyCardsAndTransctionHistorySection(),
+          SizedBox(
+            height: 24,
+          ),
+          IncomeSection(),
+        ],
+      ),
+    ),
           ),
         ),
       ],
